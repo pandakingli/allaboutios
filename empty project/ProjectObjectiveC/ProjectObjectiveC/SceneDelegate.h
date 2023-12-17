6d38fe04f8f75c85f9944ab55cdfc053c9ae8e05
pandakingli <pandakingli@outlook.com>
2023-12-17 10:12:35 +0800

    add empty projects

---
+++ empty project/ProjectObjectiveC/ProjectObjectiveC/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  ProjectObjectiveC
+//
+//  Created by pandakingli on 2023/12/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ empty project/ProjectObjectiveC/ProjectObjectiveC/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ProjectObjectiveC
+//
+//  Created by pandakingli on 2023/12/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
