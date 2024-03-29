92435139d28fdf1901d691b9358a53265ae23564
pandakingli <pandakingli@outlook.com>
2024-03-29 21:10:51 +0800

    add swift

---
+++ empty project/ProjectSwift/ProjectSwift/RootVC.swift
@@ -0,0 +1,29 @@
+//
+//  RootVC.swift
+//  ProjectSwift
+//
+//  Created by 李宁 on 2024/3/29.
+//
+
+import UIKit
+
+class RootVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
