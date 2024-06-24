899c10d2517c63bfb95f4a20fc5c1b41f0aebbf3
pandakingli <pandakingli@outlook.com>
2024-06-24 19:05:31 +0800

    add

--- web/webpack1use/web1changelinecolor/webpack.config.js
+++ web/webpack1use/web1changelinecolor/webpack.config.js
@@ -1,6 +1,20 @@
 //使用Node.js中的导出语法，向外导出一个webpack的配置对象
-module.exports = {
+/*
+const path = require('path')
 
+module.exports = {
+     entry: path.join(__dirname, './src/index.js'),
+     output: {
+          path: path.join(__dirname, './dist'),
+          filename: 'bundle.js'
+     },
     //webpack的运行模式，development开发模式，production
     mode: 'development'
+}
+*/
+
+module.exports = {
+
+   //webpack的运行模式，development开发模式，production
+   mode: 'development'
 }
